d8c9acb2741d03a2166f99989fb874fc8e181b81
Xethrus <80862366+Xethrus@users.noreply.github.com>
2022-02-26 09:53:07 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
+#include "raylib.h"
+#include <iostream>
+#include <vector>
+#include <string>
 
+
+const float SCREEN_W = 600;
+const float SCREEN_H = 600;
+
+class Word
+{
+public:
+   std::vector<std::string> allWords;
+   std::vector<char> wordChars;
+   Word(std::string word) : allWords{word} {};
+};
