fd7cfd1437182e5497168b0099cfc658ccc1ddc0
Xethrus <80862366+Xethrus@users.noreply.github.com>
2022-02-27 23:12:11 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ const float SCREEN_H = 600;
 class Word
 {
    std::string wordChars;
+   
    bool isPressed(char pressedKey, char currentIndex)
    {
       if(IsKeyPressed(pressedKey) == currentIndex)
@@ -17,6 +18,7 @@ class Word
          return true;
       } else { return false; }
    }
+
    void iterateWord(Word currentWord)
    {
       for(int i = 0; i == currentWord.wordChars.length(); i++)
