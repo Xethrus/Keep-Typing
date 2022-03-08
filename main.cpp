ac25ce5c6f56aefb4cf9036ff10b42e3a822db47
Xethrus <80862366+Xethrus@users.noreply.github.com>
2022-03-07 19:39:47 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -65,9 +65,9 @@ int main(void){
    //Main loop
    while (!WindowShouldClose())
    {
-      std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-      test ++;
-      std::cout << test << std::endl;
+//       std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+//       test ++;
+//       std::cout << test << std::endl;
       //update
       typedChar(checkPIndex(paragraph));
 
